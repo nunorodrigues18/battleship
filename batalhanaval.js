b8b0a838829d8a2e3aa0c4e9497064c3a72cba4f
nunorodrigues18 <36621629+nunorodrigues18@users.noreply.github.com>
2018-04-16 09:47:07 +0100

    Update batalhanaval.js

--- batalhanaval.js
+++ batalhanaval.js
@@ -1,17 +1,20 @@
-    // Gerar um valor inteiro aleatório entre 1 e 5. (Porquê?)
+// Gerar um valor inteiro aleatório entre 1 e 5. (Porque o navio ao estar localizado em 3 quadrados consecutivos, o último qu8e este pode ocupar é o 5).)
 
-var posicaoAleatoria = 1 + Math.floor(Math.random() * 5);
+var posicaoAleatoria = 1 + Math.floor(Math.random() * 5); //A posição inicial passa de fixa para aleatória.
 
-var parte1 = posicaoAleatoria;
-var parte2 = parte1+1;
-var parte3 = parte2 +1;
+//Definição das variável das localizações dos quadrados onde se localiza o navio:
+var posicao1 = posicaoAleatoria; //A variável posicao1 é definida aleatoriamente na linha dos 7 quadrados.
+var posicao2 = posicao1+1; //A variável posicao 2 é definida por ser aquela que vem imediatamente à variável aleatória posicao1.
+var posicao3 = posicao2 +1; //A variável posicao3 é definida assim como a variavel posicao2 como a que se soma um a posicao anterior do navio.
 
-var jogada;
-var atingidas = 0;
-var totalJogadas = 0;
+//Inicialização de variáveis:
+var jogada; //Definição da variável jogada.
+var atingidas = 0; //Inicialização da variável aingidas a zero.
+var totalJogadas = 0; //Inicialização da variável totalJogadas a zero.
 
-var foiAfundado = false;
+var foiAfundado = false; //Inicialização da variável foiAfundado a falso.
 
+//O Ciclo while ao ser uma forma de iterar sobre um conjunto de valores até algo acontecer, neste caso até 
 
 while (foiAfundado == false) {
 
@@ -25,7 +28,7 @@ while (foiAfundado == false) {
 
       totalJogadas = totalJogadas + 1;
 
-      if (jogada == parte1 || jogada == parte2 || jogada == parte3) {
+      if (jogada == posicao1 || jogada == posicao2 || jogada == posicao3) {
 
          alert("Acertaste em mais uma parte!");
 
